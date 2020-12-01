57215115f4217ccf68c8985d9f0d77e027017357
99Douglas <99douglasmesquita@gmail.com>
2020-12-01 17:35:33 -0300

    Problema de requisições múltiplas resolvido.

--- pagina_inicial/js/Controller.js
+++ pagina_inicial/js/Controller.js
@@ -1,16 +1,5 @@
 class Controller {
 
-    constructor() {
-        [...document.querySelectorAll('.cards-filmes')].forEach(function(card) {
-            card.addEventListener("click", () => {
-                let filme = card.id;
-    
-                let controller = new Controller();
-                controller.procuraEMostraFilme(filme);
-            })
-        })
-    }
-
     procuraEMostraFilme (filme) {
 
         let model = new Model()
@@ -20,5 +9,14 @@ class Controller {
         });
     }
 }
+
+[...document.querySelectorAll('.cards-filmes')].forEach(function(card) {
+    card.addEventListener("click", () => {
+        let filme = card.id;
+
+        let controller = new Controller();
+        controller.procuraEMostraFilme(filme);
+    })
+})
 
 let controlar = new Controller()
